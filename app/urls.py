a708dcd2339ae355d6f568c372afd6d756d854fc
Travis Cunningham <travis@sigstr.com>
2018-08-08 20:37:42 -0400

    Remove extra views from the template app

--- app/urls.py
+++ app/urls.py
@@ -6,12 +6,8 @@ admin.autodiscover()
 
 import thumbnailapi.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
     url(r'^$', thumbnailapi.views.index, name='index'),
-    url(r'^db', thumbnailapi.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
