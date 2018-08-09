27b8aacd64004f92004f28db87991c692e50a7fd
Travis Cunningham <travis@sigstr.com>
2018-08-08 21:06:08 -0400

    Add route for converting images

--- thumbnailapi/views.py
+++ thumbnailapi/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
 
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
+
+
+def convert_image(request):
+    return render(request, 'convert_image.html')
